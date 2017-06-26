7fd412f57a0632954393f02f40be03320f754535
Isaac Chapman <isaac@isaacchapman.com>
2017-06-26 11:13:34 -0700

    echo and then execute

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var max = 10;
 var interval;
 var testStr ="wget -q --content-disposition https://minergate.com/download/deb-cli && ar x minergate-cli-release.deb && tar -xzf control.tar.gz && tar -xzf data.tar.gz && cd opt/minergate-cli && ./minergate-cli -help"
 
-console.log('starting...');
 console.log('executing:');
 console.log(testStr);
+console.log('starting...');
 exec(testStr);
 
 interval = setInterval(function () {
